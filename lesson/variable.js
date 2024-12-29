bf0d34ec3832717b056493f893c83959fa38aad3
Hao Mai <maigiahao1209@gmail.com>
2024-12-29 13:47:35 +0700

    Commit

---
+++ lesson/variable.js
@@ -0,0 +1,21 @@
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);
+
+
+console.log(typeof (3));
+console.log(typeof 3);
+
+//camelCase
+const myName = "John";
+//snake_case
+const my_name = "John";
+//PascalCase
+const MyName = "John";
+
+//Rounding
+let rounddingNearest = Math.round(9/2);
+console.log(rounddingNearest);
+
+//Rounding down
+let roundingDown = Math.floor(9/2);
+console.log(roundingDown);
