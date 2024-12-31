cc42dfb4c73283bbb96d6a219ec42c999f40bb46
Hao Mai <maigiahao1209@gmail.com>
2024-12-31 19:30:11 +0700

    Commit

--- labs/lab_2/lab_2_2.js
+++ labs/lab_2/lab_2_2.js
@@ -1,5 +1,5 @@
 const readline = require("readline-sync")
 
 let number = Number(readline.question('Input your number: '));
-let result = (number % 2 == 0 ? "Even number" : "Odd number");
+let result = (number % 2 === 0 ? "Even number" : "Odd number");
 console.log(result);
