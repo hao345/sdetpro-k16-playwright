7205e7cc66d1cd166f0263bbe20054e0fc086a16
Hao Mai <maigiahao1209@gmail.com>
2025-01-11 21:24:05 +0700

    Commit

--- labs/lab_3/UtilMethods.js
+++ labs/lab_3/UtilMethods.js
@@ -1,14 +1,27 @@
-module.exports = {isEvenNumber,isOddNumber, input}
-const readline = require ('readline-sync')
+module.exports = { isEvenNumber, isOddNumber, input , sort}
+const readline = require('readline-sync')
 
-function isEvenNumber(number){
+function isEvenNumber(number) {
     return number % 2 === 0
 }
 
-function isOddNumber(number){
+function isOddNumber(number) {
     return number % 2 !== 0
 }
- 
-function input(){
+
+function input() {
     return Number(readline.question('Input:'))
+}
+
+function sort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let z = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = z;
+            }
+        }
+    }
+    return arr;
 }
--- labs/lab_3/lab_3_3.js
+++ labs/lab_3/lab_3_3.js
@@ -1,12 +1,5 @@
-let arr = [12, 34, 1, 16, 28];
+const {sort} = require('./UtilMethods')
+let arr = [12, 34, 1, 16, 28, -2];
 
-for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1 - i; j++) {
-        if (arr[j] > arr[j + 1]) {
-            let z = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = z;
-        }
-    }
-}
-console.log(arr)
+let sortNumber = sort(arr)
+console.log(sortNumber)
